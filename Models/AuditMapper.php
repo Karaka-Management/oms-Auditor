fab36623f9d32d10bbf34fbc5b2c34cae3c7f362
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-22 18:42:34 +0100

    implement localization

--- Models/AuditMapper.php
+++ Models/AuditMapper.php
@@ -61,6 +61,14 @@ final class AuditMapper extends DataMapperAbstract
     ];
 
     /**
+     * Model to use by the mapper.
+     *
+     * @var string
+     * @since 1.0.0
+     */
+    protected static string $model = Audit::class;
+
+    /**
      * Primary table.
      *
      * @var string
