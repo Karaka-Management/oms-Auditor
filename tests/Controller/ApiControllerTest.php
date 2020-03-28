52b65ff638f1a064e1777bb1564dddb6c8708358
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-03-28 16:27:02 +0100

    test path fixes

--- tests/Controller/ApiControllerTest.php
+++ tests/Controller/ApiControllerTest.php
@@ -14,8 +14,6 @@ declare(strict_types=1);
 
 namespace Modules\Audit\tests\Controller;
 
-require_once __DIR__ . '/../../tests/../Autoloader.php';
-
 use Model\CoreSettings;
 
 use Modules\Admin\Models\AccountPermission;
