344c3aee773575b28b2bed6e136e0748660ee8b4
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-07-31 15:44:05 +0200

    update composer and fix tests and add gitignore

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -28,48 +28,4 @@ return [
             ],
         ],
     ],
-    '^.*/admin/audit/module/list.*$' => [
-        [
-            'dest' => '\Modules\Auditor\Controller\BackendController:viewAuditorModuleList',
-            'verb' => RouteVerb::GET,
-            'permission' => [
-                'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::AUDIT,
-            ],
-        ],
-    ],
-    '^.*/admin/audit/module/single.*$' => [
-        [
-            'dest' => '\Modules\Auditor\Controller\BackendController:viewAuditorModuleSingle',
-            'verb' => RouteVerb::GET,
-            'permission' => [
-                'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::AUDIT,
-            ],
-        ],
-    ],
-    '^.*/admin/audit/account/list.*$' => [
-        [
-            'dest' => '\Modules\Auditor\Controller\BackendController:viewAuditorAccountList',
-            'verb' => RouteVerb::GET,
-            'permission' => [
-                'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::AUDIT,
-            ],
-        ],
-    ],
-    '^.*/admin/audit/account/single.*$' => [
-        [
-            'dest' => '\Modules\Auditor\Controller\BackendController:viewAuditorAccountSingle',
-            'verb' => RouteVerb::GET,
-            'permission' => [
-                'module' => BackendController::MODULE_NAME,
-                'type'  => PermissionType::READ,
-                'state' => PermissionState::AUDIT,
-            ],
-        ],
-    ],
 ];
