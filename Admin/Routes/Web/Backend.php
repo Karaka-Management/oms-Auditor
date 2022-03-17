af2137e53acebbcb928a0e12bca98efe5e9e44ca
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-03-17 23:01:29 +0100

    changed badly named permission type to permission category because type should only be read/write/...

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -13,7 +13,7 @@
 declare(strict_types=1);
 
 use Modules\Auditor\Controller\BackendController;
-use Modules\Auditor\Models\PermissionState;
+use Modules\Auditor\Models\PermissionCategory;
 use phpOMS\Account\PermissionType;
 use phpOMS\Router\RouteVerb;
 
@@ -25,7 +25,7 @@ return [
             'permission' => [
                 'module' => BackendController::NAME,
                 'type'   => PermissionType::READ,
-                'state'  => PermissionState::AUDIT,
+                'state'  => PermissionCategory::AUDIT,
             ],
         ],
     ],
@@ -36,7 +36,7 @@ return [
             'permission' => [
                 'module' => BackendController::NAME,
                 'type'   => PermissionType::READ,
-                'state'  => PermissionState::AUDIT,
+                'state'  => PermissionCategory::AUDIT,
             ],
         ],
     ],
