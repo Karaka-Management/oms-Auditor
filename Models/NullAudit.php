16520b48485d84fb4bbe6017d94f8e7e8ed07745
Dennis Eichhorn <spl1nes.com@googlemail.com>
2022-11-09 23:02:52 +0100

    reduce serialization of null models

--- Models/NullAudit.php
+++ Models/NullAudit.php
@@ -36,4 +36,12 @@ final class NullAudit extends Audit
         $this->id = $id;
         parent::__construct();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function jsonSerialize() : mixed
+    {
+        return ['id' => $this->id];
+    }
 }
